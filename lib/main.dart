2a858ef774c9be761c3af493ab7fd568129e6c97
intenf from bigboss <taichi.nakamura2104@gmail.com>
2020-11-07 17:09:36 +0900

    profile ver 1 終了

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/prof_image.png'),
+              ),
+              Text(
+                "Taichi Nakamura",
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 50,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "DATA SCIENTIST",
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
